8b0e5f81f2f7324a989fd82e8309fdb5ff2482fb
Ruslan Mingaleev <neoch56@mail.ru>
2023-07-12 23:04:16 +0300

    Add QueryClientProvider

--- app/providers/MainProvider.tsx
+++ app/providers/MainProvider.tsx
@@ -1,17 +1,28 @@
 import { FC, PropsWithChildren } from 'react'
 import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from 'next-themes'
 import HeadProvider from './HeadProvider/HeadProvider'
 import { store } from '@/store/store'
 
 import Layout from '@/components/layout/Layout'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
 const MainProvider: FC<PropsWithChildren> = ({ children }) => {
   return (
     <ThemeProvider>
       <HeadProvider>
         <Provider store={store}>
-          <Layout>{children}</Layout>
+          <QueryClientProvider client={queryClient}>
+            <Layout>{children}</Layout>
+          </QueryClientProvider>
         </Provider>
       </HeadProvider>
     </ThemeProvider>
