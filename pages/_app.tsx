1c2ee3d2758b621bf7055974de0419c837450fda
Ruslan Mingaleev <neoch56@mail.ru>
2023-07-12 13:11:46 +0300

    Add global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
 import MainProvider from 'providers/MainProvider'
 
+import '@/assets/styles/globals.scss'
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MainProvider>
